20f8dc119dd65f90cd0bd5f337ad3a3d14878e77
LucasJeanpierre <61747163+LucasJeanpierre@users.noreply.github.com>
2022-10-15 16:15:39 +0200

    Feature: Prototype TimeManager working

---
+++ Assets/Scripts/Gameplay/TimeManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+
+    [SerializeField] private float Time = 0.0f;
+    public Text TimeText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Display the current time on the UI
+        TimeText.text = "Time : " + Time.ToString();
+
+        //Change the time manually
+        #if (UNITY_EDITOR)
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Time += 1.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Time -= 1.0f;
+        }
+        #endif
+    }
+
+    /**
+     * This is the function that allow all the other objects of the game the get the current time
+     * The time variable is private to prevent other object from modifying it
+     * */
+    public float GetTime()
+    {
+        return Time;
+    }
+}
