56732afaf5d2261c28b099f0f647e28d6b655c5c
LucasJeanpierre <61747163+LucasJeanpierre@users.noreply.github.com>
2022-11-29 18:41:45 +0100

    Feature: Transition between scene after finish lvl

--- Assets/EndLevelScript.cs
+++ Assets/EndLevelScript.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndLevelScript : MonoBehaviour
 {
 
     private BoxCollider2D _boxCollider2D;
     // Start is called before the first frame update
+
+    public string nextLevel;
     void Start()
     {
         _boxCollider2D = GetComponent<BoxCollider2D>();
@@ -18,11 +21,18 @@ public class EndLevelScript : MonoBehaviour
 
     }
 
+    public void GoToNextLevel() {
+        SceneManager.LoadScene("Scenes/Levels/"+nextLevel);
+    }
+
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("End Level");
+            GoToNextLevel();
         }
     }
 
