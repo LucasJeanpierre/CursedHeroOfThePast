1ca861a79f8598f44b4cf0461244a406f6db1271
LucasJeanpierre <61747163+LucasJeanpierre@users.noreply.github.com>
2022-12-01 13:32:24 +0100

    Feature: rewind animation for the clone

--- Assets/Scripts/Gameplay/PlayerOnRewind.cs
+++ Assets/Scripts/Gameplay/PlayerOnRewind.cs
@@ -24,23 +24,38 @@ public class PlayerOnRewind : MonoBehaviour
     private float currentTimeRewind;
 
     private bool _isStatic;
-    
+
+    internal Animator animator;
+
+    public float maxSpeed = 7;
+
+    private SpriteRenderer _spriteRenderer;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         _transform = this.transform;
         _rigidbody2D = this.GetComponent<Rigidbody2D>();
         _rewindSaveInfo = GetComponent<RewindSaveInfo>();
         //currentTimeRewind = (float)System.Math.Round(Time.time, 2);
         currentTimeRewind -= currentTimeRewind % 0.02f;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         //Debug.Log(_rewindSaveInfo);
+    }
+
+    void Update()
+    {
+
+
     }
 
     // Update is called once per frame
     public void FixedUpdate()
     {
-        if (!_isStatic) {
+        if (!_isStatic)
+        {
             Rewind();
         }
     }
@@ -51,6 +66,22 @@ public class PlayerOnRewind : MonoBehaviour
         //_rewindSaveInfo.GetTimeRewindObject(currentTimeRewind);
         try
         {
+            if (_transform.position.x > GetTimeRewindObject(_timeManager.GetCustomTime()).GetPosition().x)
+                _spriteRenderer.flipX = false;
+            else
+                _spriteRenderer.flipX = true;
+
+            if (Math.Abs(_transform.position.y - GetTimeRewindObject(_timeManager.GetCustomTime()).GetPosition().y) < 0.01f)
+            {
+                animator.SetBool("grounded", true);
+                animator.SetFloat("velocityX", Mathf.Abs((_transform.position.x - GetTimeRewindObject(_timeManager.GetCustomTime()).GetPosition().x) / Time.deltaTime) / maxSpeed);
+            }
+            else
+            {
+                animator.SetBool("grounded", false);
+            }
+
+
             _rigidbody2D.MovePosition(GetTimeRewindObject(_timeManager.GetCustomTime()).GetPosition());
         }
         catch (System.Exception)
@@ -63,7 +94,8 @@ public class PlayerOnRewind : MonoBehaviour
         currentTimeRewind = (float) System.Math.Round(currentTimeRewind, 2);
         currentTimeRewind -= currentTimeRewind % 0.02f;*/
 
-        if ((_timeManager.GetCustomTime() < _timeManager.getMaxTime() - _timeManager.getTimeRewindLimit()) || (_timeManager.GetCustomTime() < 0f)) {
+        if ((_timeManager.GetCustomTime() < _timeManager.getMaxTime() - _timeManager.getTimeRewindLimit()) || (_timeManager.GetCustomTime() < 0f))
+        {
             _playerController.StopRewinding();
         }
 
@@ -77,9 +109,9 @@ public class PlayerOnRewind : MonoBehaviour
         _rewindList = new Dictionary<float, TimeRewindObject>();
 
         //recreate a new list with all the elements of the list in parameter
-        foreach (KeyValuePair<float,TimeRewindObject> element in rewindSaveInfo.GetRewindList())
+        foreach (KeyValuePair<float, TimeRewindObject> element in rewindSaveInfo.GetRewindList())
         {
-            _rewindList.Add((float) element.Key, element.Value);
+            _rewindList.Add((float)element.Key, element.Value);
             //Debug.Log("Added: " + element.Key + " | " + element.Value.GetPosition());
         }
 
@@ -89,12 +121,14 @@ public class PlayerOnRewind : MonoBehaviour
         //Debug.Log("set rewind save info");
     }
 
-    public void SetCurrentRewindTime(float new_currentRewindTime) {
-        currentTimeRewind = (float) System.Math.Round(new_currentRewindTime, 2);
+    public void SetCurrentRewindTime(float new_currentRewindTime)
+    {
+        currentTimeRewind = (float)System.Math.Round(new_currentRewindTime, 2);
         currentTimeRewind -= currentTimeRewind % 0.02f;
     }
 
-    public void setTimeManager(TimeManager newTimeManager) {
+    public void setTimeManager(TimeManager newTimeManager)
+    {
         //Debug.Log("set the time manager");
         _timeManager = newTimeManager;
         //_rewindSaveInfo._timeManager = _timeManager;
@@ -106,9 +140,9 @@ public class PlayerOnRewind : MonoBehaviour
         //iterate through the list to find the time
         foreach (KeyValuePair<float, TimeRewindObject> element in _rewindList)
         {
-            float result = (float) element.Key - (float) time;
-            
-            if ((float) System.Math.Round(result, 2) == 0.00f)
+            float result = (float)element.Key - (float)time;
+
+            if ((float)System.Math.Round(result, 2) == 0.00f)
             {
                 //Debug.Log(element.Value);
                 return element.Value;
