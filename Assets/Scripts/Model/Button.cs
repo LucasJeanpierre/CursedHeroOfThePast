e6bb740ae43d57e31333fffe4de7f71ce4f1ca69
nergs33700 <92976135+nergs33700@users.noreply.github.com>
2023-01-05 22:06:35 +0100

    button_finally_working

--- Assets/Scripts/Model/Button.cs
+++ Assets/Scripts/Model/Button.cs
@@ -31,14 +31,20 @@ public class Button : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-         PressButton();
+         Collider2D playerCollider = other.gameObject.GetComponent<Collider2D>();
+         _isAbove= collideAbove(playerCollider);
+         _left=false;
+         if (_isAbove){
+            PressButton();
+         }
         
     }
 
     public void PressButton()
     {
-        _isAbove=true;
-        _left=false;
+        
+        _isPressed=true;
+        _animator.SetBool("Button_Pressed",_isPressed);
         StopCoroutine("unPressButtonAfterDelay");
         // GetComponent<SpriteRenderer>().color = Color.red;
         _animator.SetBool("Someone_Above",_isAbove);
@@ -74,8 +80,7 @@ public class Button : MonoBehaviour
     {
         UpdateCollider();
         // Debug.Log(_isPressed);
-        _isPressed= !(_left);
-        _animator.SetBool("Button_Pressed",_isPressed);
+        
     }
 
     public void unPressButton()
@@ -83,6 +88,8 @@ public class Button : MonoBehaviour
         
         // GetComponent<SpriteRenderer>().color = Color.blue;
         if (!_isAbove){
+            _isPressed=false;
+            _animator.SetBool("Button_Pressed",_isPressed);
             _left=true;
             _animator.SetBool("Someone_Left",_left);
         }
@@ -119,6 +126,24 @@ public class Button : MonoBehaviour
         // float x_dim=boxCollider.size.x;
         boxCollider.size=new Vector2(S.x,S.y);
         boxCollider.offset=new Vector2 (0,offset_y );
+    }
+
+    private bool collideAbove(Collider2D playerCollider){
+
+        
+        float y_inf_player=playerCollider.bounds.min.y;
+
+        BoxCollider2D button_collider= this.gameObject.GetComponent<BoxCollider2D>();
+        float y_max_button=button_collider.bounds.max.y;
+
+        if (y_max_button<=y_inf_player){
+            return true;
+        }
+        return false;
+
+        
+
+
     }
 
 
