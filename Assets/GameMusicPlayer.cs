a4d7931ea572caa0f17af9e12f9d8a259fb40217
BaptisteS <baptiste.sourisseau@etudiant.univ-rennes1.fr>
2023-01-08 15:18:20 +0100

    Fix the music

---
+++ Assets/GameMusicPlayer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusicPlayer : MonoBehaviour
+{
+
+    private static GameMusicPlayer instance = null;
+    public static GameMusicPlayer Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
